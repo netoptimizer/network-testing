d629cb3afa9daf7c3877dd412506d9c0ef780a24
Jesper Dangaard Brouer <brouer@redhat.com>
2012-08-15 13:42:41 +0200

    Adding a simple IPv6 example.
    Slightly modified version from stackoverflow:
     http://stackoverflow.com/questions/2455762/why-cant-i-bind-ipv6-socket-to-a-linklocal-address
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

---
+++ src/ipv6_example01.c
@@ -0,0 +1,52 @@
+/* Simple IPv6 example, originally borrowed from stackoverflow:
+    http://stackoverflow.com/questions/2455762/why-cant-i-bind-ipv6-socket-to-a-linklocal-address
+ */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void error(char *msg)
+{
+	perror(msg);
+	exit(0);
+}
+int main(int argc, char *argv[])
+{
+	int sock, length, fromlen, n;
+	struct sockaddr_in6 server;
+	struct sockaddr_in6  from;
+
+	int portNr = 5555;
+	char buf[1024];
+
+	length = sizeof (struct sockaddr_in6);
+
+	sock=socket(AF_INET6, SOCK_DGRAM, 0);
+	if (sock < 0) error("Opening socket");
+
+	bzero((char *)&server, length);
+	server.sin6_family=AF_INET6;
+	server.sin6_addr=in6addr_any;
+	server.sin6_port=htons(portNr);
+
+	inet_pton( AF_INET6, "::", (void *)&server.sin6_addr.s6_addr);
+	//inet_pton( AF_INET6, "fe80::21f:29ff:feed:2f7e", (void *)&server.sin6_addr.s6_addr);
+	//inet_pton( AF_INET6, "::1", (void *)&server.sin6_addr.s6_addr);
+
+	if (bind(sock,(struct sockaddr *)&server,length)<0)
+		error("binding");
+	fromlen = sizeof(struct sockaddr_in6);
+	while (1) {
+		n = recvfrom(sock,buf,1024,0,(struct sockaddr *)&from,&fromlen);
+		if (n < 0) error("recvfrom");
+		write(1,"Received a datagram: ",21);
+		write(1,buf,n);
+		n = sendto(sock,"Got your message\n",17,
+			   0,(struct sockaddr *)&from,fromlen);
+		if (n  < 0) error("sendto");
+        }
+}
