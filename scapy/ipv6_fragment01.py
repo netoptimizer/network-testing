36d901c4989287b55a232a71fbfce247604b6c32
Jesper Dangaard Brouer <brouer@redhat.com>
2012-08-16 22:38:36 +0200

    scapy: extend the ipv6_fragment01.py script to send four IPv6 fragments.
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- scapy/ipv6_fragment01.py
+++ scapy/ipv6_fragment01.py
@@ -12,6 +12,8 @@ from scapy.all import *
 eth_fvm05=Ether(dst="52:54:bb:cc:dd:05") # mac of fvm05%eth1
 eth_fvm03=Ether(dst="52:54:bb:cc:dd:03") # mac of fvm03%eth1
 
+dst_port=5555
+
 #IPv6 parameters
 sip="fee0:200::106"
 #dip="fee0:200::42"
@@ -21,43 +23,54 @@ dip="fee0:cafe::42"
 conf.route6.add("fee0:cafe::/64",gw="fee0:200::1")
 
 payload1="AAAAAAAA"
+payload2="BBBBBBBB"
+payload3="CCCCCCCC"
+payload_all="AAAAAAAABBBBBBBBCCCCCCCC"
 ipv6_1=IPv6(src=sip, dst=dip, plen=16)
-#ipv6_1=IPv6(src=sip, dst=dip, plen=24)
 #icmpv6=ICMPv6EchoRequest(cksum=0x7d2b) 
 
 # Calculate/find the correct checksum
 #  by constructing the NONE-fragmented UDP packet
-csum_udp0=UDP(sport=12345, dport=5001)
-csum_pkt0=ipv6_1/csum_udp0/payload1
+csum_udp0=UDP(sport=12345, dport=dst_port)
+csum_pkt0=ipv6_1/csum_udp0/payload_all
 #
 # Scapy hack to force chksum calculation
 del csum_pkt0.chksum
 csum_pkt0 = csum_pkt0.__class__(str(csum_pkt0))
 #csum_pkt0.show()
+csum_pkt0.show2()
 print "chksum:", hex(csum_pkt0.chksum)
 
 # Need to set the chksum explicitly, as it cannot be calculated
 #  correctly as we construct fragments here 
-udp=UDP(sport=12345, dport=5001, chksum=csum_pkt0.chksum, len=16)
+udp=UDP(sport=12345, dport=dst_port, chksum=csum_pkt0.chksum, len=32)
 
 #Fragment 
 # http://www.tcpipguide.com/free/t_IPv6DatagramMainHeaderFormat-2.htm
 # next-header = nh = 58 = ICMPv6
 # next-header = nh = 17 = UDP
+# offset is multiplum of 8
+# "m" == More fragments to follow
 frag1=IPv6ExtHdrFragment(offset=0, m=1, id=502, nh=17) 
-frag2=IPv6ExtHdrFragment(offset=1, m=0, id=502, nh=17) 
+frag2=IPv6ExtHdrFragment(offset=1, m=1, id=502, nh=17) 
+frag3=IPv6ExtHdrFragment(offset=2, m=1, id=502, nh=17) 
+frag4=IPv6ExtHdrFragment(offset=3, m=0, id=502, nh=17) 
 
 #packet1=ipv6_1/frag1/icmpv6 
 packet1=ipv6_1/frag1/udp
-#packet1=ipv6_1/frag1/udp/payload1
-packet2=ipv6_1/frag2/payload1 
+packet2=ipv6_1/frag2/payload1
+packet3=ipv6_1/frag3/payload2
+packet4=ipv6_1/frag4/payload3
 
 #send(packet1) 
 #send(packet2)
 print "Sending fragmented UDP packet(s)"
+# Notice, not sending packets in right order, BUT when a hosts
+#  receives the packets, they have been reordered... hmmm?!
+sendp(eth_fvm03/packet3)
 sendp(eth_fvm03/packet2)
-#delay(2)
 sendp(eth_fvm03/packet1) 
+sendp(eth_fvm03/packet4) 
 
 #packet1.show()
 
