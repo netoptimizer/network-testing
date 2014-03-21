d1fcdd2188d2ee946a2eac9d94f20fdfbf95d685
Jesper Dangaard Brouer <brouer@redhat.com>
2014-03-21 11:05:01 +0100

    src: introduce common.{c,h} for shared functions
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

---
+++ src/common.c
@@ -0,0 +1,11 @@
+/* -*- c-file-style: "linux" -*-
+ * Author: Jesper Dangaard Brouer <netoptimizer@brouer.com>, (C)2014
+ * License: GPLv2
+ * From: https://github.com/netoptimizer/network-testing
+ *
+ * Common/shared helper functions
+ *
+ */
+#include <stdint.h>
+
+#include "global.h"
---
+++ src/common.h
@@ -0,0 +1,20 @@
+/* -*- c-file-style: "linux" -*-
+ * Author: Jesper Dangaard Brouer <netoptimizer@brouer.com>, (C)2014
+ * License: GPLv2
+ * From: https://github.com/netoptimizer/network-testing
+ *
+ * Common/shared helper functions
+ */
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdint.h>
+
+inline uint64_t rdtsc()
+{
+	uint32_t low, high;
+	asm volatile("rdtsc" : "=a" (low), "=d" (high));
+	return low  | (((uint64_t )high ) << 32);
+}
+
+#endif /* COMMON_H */
