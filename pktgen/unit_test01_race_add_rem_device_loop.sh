f08b8b90731dbb0a867d1a8c1d394d796ad14dfb
Jesper Dangaard Brouer <brouer@redhat.com>
2014-05-14 13:28:38 +0200

    pktgen: unit_test01 detailed description of race
    In pktgen there is several kernel threads, but there is only one CPU
    running each kernel thread.  Communication with the kernel threads are
    done through some thread control flags.  This allow the thread to
    change data structures at a know synchronization point, see main
    thread func pktgen_thread_worker().
    
    Userspace changes are communicated through proc-file writes.  There
    are three types of changes, general control changes "pgctrl"
    (func:pgctrl_write), thread changes "kpktgend_X"
    (func:pktgen_thread_write), and interface config changes "etcX@N"
    (func:pktgen_if_write).
    
    Userspace "pgctrl" and thread changes are synchronized via the mutex
    pktgen_thread_lock, thus only a single userspace instance can run.
    The mutex is taken while the packet generator is running, by pgctrl
    "start".  Thus e.g. "add_device" cannot be invoked when pktgen is
    running/started.
    
    All "pgctrl" and all "thread" changes, except thread "add_device",
    communicate via the thread control flags.  The main problem is the
    exception "add_device", that modifies threads "if_list" directly.
    
    Fortunately "add_device" cannot be invoked while pktgen is running.
    But there exists a race between "rem_device_all" and "add_device"
    (which normally don't occur, because "rem_device_all" waits 125ms
    before returning). Background'ing "rem_device_all" and running
    "add_device" immediately allow the race to occur.
    
    The race affects the threads (list of devices) "if_list".  The if_lock
    is used for protecting this "if_list".  Other readers are given
    lock-free access to the list under RCU read sections.
    
    Note, interface config changes (via proc) can occur while pktgen is
    running, which worries me a bit.  I'm assuming proc_remove() takes
    appropriate locks, to assure no writers exists after proc_remove()
    finish.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- pktgen/unit_test01_race_add_rem_device_loop.sh
+++ pktgen/unit_test01_race_add_rem_device_loop.sh
@@ -17,6 +17,48 @@ root_check_run_with_sudo "$@"
 source ${basedir}/parameters.sh
 source ${basedir}/config.sh
 
+# Detailed description of race
+# ============================
+# (race requires understanding pktgen's thread model, desc below)
+#
+# In pktgen there is several kernel threads, but there is only one CPU
+# running each kernel thread.  Communication with the kernel threads are
+# done through some thread control flags.  This allow the thread to
+# change data structures at a know synchronization point, see main
+# thread func pktgen_thread_worker().
+#
+# Userspace changes are communicated through proc-file writes.  There
+# are three types of changes, general control changes "pgctrl"
+# (func:pgctrl_write), thread changes "kpktgend_X"
+# (func:pktgen_thread_write), and interface config changes "etcX@N"
+# (func:pktgen_if_write).
+#
+# Userspace "pgctrl" and thread changes are synchronized via the mutex
+# pktgen_thread_lock, thus only a single userspace instance can run.
+# The mutex is taken while the packet generator is running, by pgctrl
+# "start".  Thus e.g. "add_device" cannot be invoked when pktgen is
+# running/started.
+#
+# All "pgctrl" and all "thread" changes, except thread "add_device",
+# communicate via the thread control flags.  The main problem is the
+# exception "add_device", that modifies threads "if_list" directly.
+#
+# (race)
+# Fortunately "add_device" cannot be invoked while pktgen is running.
+# But there exists a race between "rem_device_all" and "add_device"
+# (which normally don't occur, because "rem_device_all" waits 125ms
+# before returning). Background'ing "rem_device_all" and running
+# "add_device" immediately allow the race to occur.
+#
+# The race affects the threads (list of devices) "if_list".  The if_lock
+# is used for protecting this "if_list".  Other readers are given
+# lock-free access to the list under RCU read sections.
+#
+# Note, interface config changes (via proc) can occur while pktgen is
+# running, which worries me a bit.  I'm assuming proc_remove() takes
+# appropriate locks, to assure no writers exists after proc_remove()
+# finish.
+
 # run if user hits control-c
 function control_c()
 {
