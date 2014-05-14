0e397829d21440c15bd02075de120b0124acc022
Jesper Dangaard Brouer <brouer@redhat.com>
2014-05-14 12:44:39 +0200

    pktgen: tune unit_test01 to hit race condition more often
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- pktgen/unit_test01_race_add_rem_device_loop.sh
+++ pktgen/unit_test01_race_add_rem_device_loop.sh
@@ -17,12 +17,26 @@ root_check_run_with_sudo "$@"
 source ${basedir}/parameters.sh
 source ${basedir}/config.sh
 
+# run if user hits control-c
+function control_c()
+{
+    echo -en "\n*** Ctrl-C ***\n"
+    kill $BACKGROUND_PID
+    exit $?
+}
+# trap keyboard interrupt (control-c)
+trap control_c SIGINT
+
 # Run this in parallel in another thread/process
 function add_remove_loop() {
     while (true); do
 	thread=0
-	add_device $DEV $thread &
+	#add_device $DEV $thread
  	remove_thread $thread &
+	sleep 0.05
+	add_device $DEV $thread
+	wait $!
+	#add_device $DEV $thread
 #	thread=2
 #	add_device $DEV $thread
 #	reset_all_threads
@@ -30,23 +44,18 @@ function add_remove_loop() {
 }
 
 function read_proc_file() {
-    for i in `seq 1 10000`; do
-	echo "Reading proc N:$i"
- 	cat /proc/net/pktgen/kpktgend_0 &
-	cat /proc/net/pktgen/$DEV@0
+    for i in `seq 1 1000`; do
+	echo -e "\n--- Reading proc files in parallel N:$i ---"
+ 	cat /proc/net/pktgen/kpktgend_0
+	if [ -e /proc/net/pktgen/$DEV@0 ]; then
+	    echo "READING interface file (to /dev/null)"
+	    cat /proc/net/pktgen/$DEV@0 > /dev/null
+	else
+	    echo "*** INTERFACE FILE DISAPPEARED ***"
+	fi
+	sleep 1
     done
 }
-
-function control_c()
-# run if user hits control-c
-{
-    echo -en "\n*** Ctrl-C ***\n"
-    kill $BACKGROUND_PID
-    exit $?
-}
-
-# trap keyboard interrupt (control-c)
-trap control_c SIGINT
 
 add_remove_loop &
 BACKGROUND_PID=$!
