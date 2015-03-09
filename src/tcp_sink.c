netoptimizer/network-testing

commit a3c4a1d4ce11181737841cf14db0bc7d413b9e96

src: tcp_sink, default enable SO_REUSEPORT and add option --no-reuseport
Enable socket option SO_REUSEPORT per default.

Adjust getopt_long to use the "flag" part of "struct option".
Introduce "flag" options "reuseport" and "no-reuseport" (which
can disable SO_REUSEPORT).  Also introduce flag option "quiet"
that will set verbose level to zero.

Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

src/tcp_sink.c
@@ -35,12 +35,16 @@
 #include "common.h"
 #include "common_socket.h"
 
+static int so_reuseport = 1;
+
 static struct option long_options[] = {
 	{"ipv4",	no_argument,		NULL, '4' },
 	{"ipv6",	no_argument,		NULL, '6' },
 	{"listen-port",	required_argument,	NULL, 'l' },
 	{"verbose",	optional_argument,	NULL, 'v' },
-	{"reuseport",	no_argument,		NULL, 's' },
+	{"quiet",	no_argument,		&verbose, 0 },
+	{"reuseport",	no_argument,		&so_reuseport, 1 },
+	{"no-reuseport",no_argument,		&so_reuseport, 0 },
 	{"write-back", 	no_argument,		NULL, 'w' },
 	{0, 0, NULL,  0 }
 };
@@ -56,10 +60,9 @@ static int usage(char *argv[])
 int main(int argc, char *argv[])
 {
 	int listenfd, connfd;
-	int option_index = 0;
+	int optind = 0;
 	int c, i;
 	int count  = 1000000;
-	int so_reuseport = 0;
 	int write_something = 0;
 	pid_t pid = getpid();
 
@@ -80,12 +83,19 @@ int main(int argc, char *argv[])
 
 	/* Parse commands line args */
 	while ((c = getopt_long(argc, argv, "c:l:64swv:",
-			long_options, &option_index)) != -1) {
+			long_options, &optind)) != -1) {
+		if (c == 0) { /* optional handling "flag" options */
+			if (verbose) {
+				printf("Flag option %s",
+				       long_options[optind].name);
+				if (optarg) printf(" with arg %s", optarg);
+				printf("\n");
+			}
+		}
 		if (c == 'c') count       = atoi(optarg);
 		if (c == 'l') listen_port = atoi(optarg);
 		if (c == '4') addr_family = AF_INET;
 		if (c == '6') addr_family = AF_INET6;
-		if (c == 's') so_reuseport= 1;
 		if (c == 'w') write_something = 1;
 		if (c == 'v') (optarg) ? verbose = atoi(optarg) : (verbose = 1);
 		if (c == '?') return usage(argv);

commit 9f3842490ca4278f5818e4d5f78092234dc02a6e

src: tcp_sink, use seperate getopt index for long options
It is wrong to use variable name "optind" as it is already
defined/used by getopt_long(3).

Fixes: a3c4a1d4ce1 ("src: tcp_sink, default enable SO_REUSEPORT and add option --no-reuseport")

Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

src/tcp_sink.c
@@ -60,7 +60,7 @@ static int usage(char *argv[])
 int main(int argc, char *argv[])
 {
 	int listenfd, connfd;
-	int optind = 0;
+	int longindex = 0;
 	int c, i;
 	int count  = 1000000;
 	int write_something = 0;
@@ -82,11 +82,11 @@ int main(int argc, char *argv[])
 
 	/* Parse commands line args */
 	while ((c = getopt_long(argc, argv, "c:l:64swv:",
-			long_options, &optind)) != -1) {
+			long_options, &longindex)) != -1) {
 		if (c == 0) { /* optional handling "flag" options */
 			if (verbose) {
 				printf("Flag option %s",
-				       long_options[optind].name);
+				       long_options[longindex].name);
 				if (optarg) printf(" with arg %s", optarg);
 				printf("\n");
 			}