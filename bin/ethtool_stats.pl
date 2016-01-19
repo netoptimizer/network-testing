eea03f8b327e27f339757a12653a5cf87a1dcac4
Jesper Dangaard Brouer <brouer@redhat.com>
2016-01-19 17:29:34 +0100

    bin: script for sampling changing adapter statistics from ethtool -S
    ethtool_stats.pl --dev DEVICE
    
    This script shows ethtool (-S|--statistics) stats, but only stats
    that changes.  And then reports stats per sec.
    
    Created this script because some driver, e.g. mlx5, report false
    stats in the ones avail via ifconfig.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

---
+++ bin/ethtool_stats.pl
@@ -0,0 +1,146 @@
+#!/usr/bin/perl -w
+
+=head1 NAME
+
+ ethtool_stats.pl - Sample changing adapter statistics from ethtool -S
+
+=head1 SYNOPSIS
+
+ ethtool_stats.pl --dev DEVICE [options]
+
+ options:
+    --dev	Ethernet adapter/device to get stats from.
+    --count	How many seconds sampling will run (default: infinite)
+    --all	List all zero stats
+    --help	Brief usage/help message.
+    --man	Full documentation.
+
+
+=head1 DESCRIPTION
+
+ This script shows ethtool (-S|--statistics) stats, but only stats
+ that changes.  And then reports stats per sec.
+
+ Created this script because some driver, e.g. mlx5, report false
+ stats via ifconfig.
+
+=cut
+
+use strict;
+use warnings FATAL => 'all';
+use Data::Dumper;
+use Pod::Usage;
+use Getopt::Long;
+
+my $DEV    = undef;
+my $debug  = 0;
+my $dumper = 0;
+my $help   = 0;
+my $man    = 0;
+my $all    = 0;
+my $count  = 0;
+
+GetOptions (
+    'dev=s'    => \$DEV,
+    'count=s'  => \$count,
+    'all!'     => \$all,
+    'debug!'   => \$debug,
+    'dumper!'  => \$dumper,
+    'help|?'   => sub { Getopt::Long::HelpMessage(-verbose => 1) },
+    'man'      => \$man,
+    ) or pod2usage(2);
+pod2usage(-exitstatus => 0, -verbose => 2) if $man;
+pod2usage(-exitstatus => 1, -verbose => 1) unless defined $DEV;
+
+my %STATS;
+
+sub collect_stats($) {
+    # Parse ethtool stats and return key=value hash
+    my $device = shift;
+    my %hash;
+    open(ETHTOOL, "sudo /usr/sbin/ethtool -S $device |");
+    while (defined(my $line = <ETHTOOL>)) {
+	chomp($line);
+	if ($line =~ m/\s*(.+):\s?(\d+)/) {
+	    my $key   = $1;
+	    my $value = $2;
+	    $hash{$key} = $value;
+	    print "PARSED: $line -- key:$key val:$value\n" if $debug;
+	} else {
+	    print "WARN: could not parse line:\"$line\"\n" if $debug;
+	}
+    }
+    close(ETHTOOL);
+    return \%hash;
+}
+
+# Example
+sub traverse_hash(%) {
+    my $hash = shift;
+    while(my ($key, $value) = each %$hash) {
+	print "key:$key val:$value\n";
+    }
+}
+# Example sort
+sub traverse_hash_sorted(%) {
+    my $hash = shift;
+    my @keys = (sort keys %$hash);
+    foreach my $key (@keys) {
+	print "key:$key val:$hash->{$key}\n";
+    }
+}
+
+sub difference($$) {
+    my ($stat, $prev)= @_;
+    my $something_changed = 0;
+    my @keys = (sort keys %$prev);
+    foreach my $key (@keys) {
+	my $value_now  = $stat->{$key};
+	my $value_prev = $prev->{$key};
+	# print "key:$key val:$prev->{$key}\n";
+	my $diff = $value_now - $value_prev;
+	next if (($diff == 0) && !$all);
+	my $pretty = $diff;
+	$pretty =~ s/(\d{1,3}?)(?=(\d{3})+$)/$1,/g;
+	# print "Stat: $key =\t $diff pretty:$pretty\n";
+	print "Ethtool($DEV) stat: \t$diff \t($pretty)\t<= $key /sec\n";
+	$something_changed++;
+    }
+    return $something_changed;
+}
+
+sub stats_loop() {
+    my $collect = $count + 1; # First round was empty (+1)
+    my $prev = {};
+    my $stats = {};
+
+    # count == 0 is infinite
+    while ( ($count == 0) ? 1 : $collect-- ) {
+	print "\nShow adapter $DEV statistics (ONLY that changed!)\n";
+	$stats = collect_stats($DEV);
+	my $changes = difference($stats, $prev);
+	if (!(keys %$prev)) {
+	    print " ***NOTE***: Collecting stats for next round\n";
+	} elsif (!$changes && !(keys %$prev)) {
+	    print " ***WARN***: No counters changed\n" ;
+	}
+	$prev = $stats;
+	sleep 1;
+    }
+}
+stats_loop();
+
+#my $HASH = collect_stats($DEV);
+#traverse_hash_sorted($HASH);
+
+#print Dumper($DEV) if $dumper;
+#print Dumper(\%STATS) if $dumper;
+#print Dumper($HASH) if $dumper;
+
+__END__
+
+=head1 AUTHOR
+
+ Jesper Dangaard Brouer <netoptimizer@brouer.com>
+
+=cut
