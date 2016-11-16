5bde8a14c921fb62cbeea2ae0fc4737a9c3da2ab
Jesper Dangaard Brouer <brouer@redhat.com>
2016-11-16 15:29:36 +0100

    bin: update netfilter_unload_modules.sh
    Update script for unloading all netfilter modules.
    
    This script is not optimal, it should at some point be
    re-written to more automatically detect which modules to
    unload and their dependencies.  For now I just did this
    manually in the script.
    
    Updated with more ipv6 modules and also unload ebtables.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- bin/netfilter_unload_modules.sh
+++ bin/netfilter_unload_modules.sh
@@ -16,20 +16,44 @@ if [ "$EUID" -ne 0 ]; then
     exit 4
 fi
 
-iptables -F ; iptables -t nat -F; iptables -t mangle -F ; ip6tables -F
-iptables -X ; iptables -t nat -X; iptables -t mangle -X ; ip6tables -X
+
+
+iptables -F ; iptables -t nat -F; iptables -t mangle -F
+iptables -X ; iptables -t nat -X; iptables -t mangle -X
 iptables -t raw -F ; iptables -t raw -X
 
-rmmod ebtable_nat  ebtables
+ip6tables -F ; ip6tables -t nat -F; ip6tables -t mangle -F
+ip6tables -X ; ip6tables -t nat -X; ip6tables -t mangle -X
+ip6tables -t raw -F ; ip6tables -t raw -X
+
+ebtables -F           ; ebtables -X
+ebtables -t nat -F    ; ebtables -t nat -X
+ebtables -t broute -F ; ebtables -t broute -X
+
+rmmod ebtable_filter ebtable_nat ebtable_broute ebtables
+rmmod ip_set
+
+rmmod ip6t_REJECT nf_reject_ipv6
+rmmod ipt_REJECT  nf_reject_ipv4
+
+rmmod ip6t_rpfilter
+
 rmmod ipt_SYNPROXY nf_synproxy_core xt_CT \
       nf_nat_masquerade_ipv4 \
       nf_conntrack_ftp nf_conntrack_tftp nf_conntrack_irc nf_nat_tftp \
       ipt_MASQUERADE nf_MASQUERADE \
-      iptable_nat nf_nat_ipv4 nf_nat nf_conntrack_ipv4 nf_nat \
-      nf_conntrack_ipv6 xt_state nf_conntrack iptable_raw \
-      nf_conntrack \
-      iptable_filter iptable_raw iptable_mangle ipt_REJECT xt_CHECKSUM \
+      iptable_nat nf_nat_ipv4 nf_conntrack_ipv4 nf_nat \
+      nf_conntrack_ipv6 xt_state iptable_raw \
+      iptable_filter iptable_raw iptable_mangle xt_CHECKSUM \
       ip_tables nf_defrag_ipv4 \
-      ip6table_filter ip6_tables nf_defrag_ipv6 ip6t_REJECT \
       xt_LOG xt_multiport \
-      xt_tcpudp xt_conntrack x_tables
+      xt_tcpudp xt_conntrack
+
+rmmod ip6table_mangle ip6table_raw ip6table_nat ip6table_filter ip6_tables
+
+rmmod nf_nat_ipv6 nf_conntrack_ipv6 nf_defrag_ipv6
+
+rmmod nf_nat nf_conntrack
+
+rmmod x_tables
+
