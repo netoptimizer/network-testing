cb6a51a0cd8fb4f80e2b95a0b4c11770e21efd5a
Jesper Dangaard Brouer <brouer@redhat.com>
2016-11-18 12:15:55 +0100

    src: adding alternative UDP flood program from Eric Dumazet
    Eric posted this program online:
     http://lkml.kernel.org/r/1479419042.8455.280.camel@edumazet-glaptop3.roam.corp.google.com
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

---
+++ src/udp_snd.c
@@ -0,0 +1,73 @@
+/* -*- c-file-style: "linux" -*-
+ *
+ * Simple UDP sender program
+ *
+ * Copyright: Eric Dumazet <eric.dumazet@gmail.com>, (C)2016
+ * License: GPLv2
+ *
+ * Quick and dirty codey. Wrote when tracking the UDP v6 checksum bug
+ * (4f2e4ad56a65f3b7d64c258e373cb71e8d2499f4 net: mangle zero checksum
+ * in skb_checksum_help()), because netperf sends the same message
+ * over and over...
+ *
+ *  Use -d 2   to remove the ip_idents_reserve() overhead.
+ */
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <error.h>
+#include <linux/errqueue.h>
+#include <netinet/in.h>
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+char buffer[1400];
+
+int main(int argc, char** argv) {
+	int fd, i;
+	struct sockaddr_in6 addr;
+	char *host = "2002:af6:798::1";
+	int family = AF_INET6;
+	int discover = -1;
+
+	while ((i = getopt(argc, argv, "4H:d:")) != -1) {
+		switch (i) {
+		case 'H': host = optarg; break;
+		case '4': family = AF_INET; break;
+		case 'd': discover = atoi(optarg); break;
+		}
+	}
+	fd = socket(family, SOCK_DGRAM, 0);
+	if (fd < 0)
+		error(1, errno, "failed to create socket");
+	if (discover != -1)
+		setsockopt(fd, SOL_IP, IP_MTU_DISCOVER,
+               &discover, sizeof(discover));
+
+	memset(&addr, 0, sizeof(addr));
+	if (family == AF_INET6) {
+		addr.sin6_family = AF_INET6;
+		addr.sin6_port = htons(9);
+		inet_pton(family, host, (void *)&addr.sin6_addr.s6_addr);
+	} else {
+		struct sockaddr_in *in = (struct sockaddr_in *)&addr;
+		in->sin_family = family;
+		in->sin_port = htons(9);
+		inet_pton(family, host, &in->sin_addr);
+	}
+	connect(fd, (struct sockaddr *)&addr,
+		(family == AF_INET6) ? sizeof(addr) :
+		sizeof(struct sockaddr_in));
+	memset(buffer, 1, 1400);
+	for (i = 0; i < 655360000; i++) {
+		memcpy(buffer, &i, sizeof(i));
+		send(fd, buffer, 100 + rand() % 200, 0);
+	}
+	return 0;
+}
