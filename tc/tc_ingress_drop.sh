389f7a80134a3718698136d3da709c7b1d0c3ffb
Jesper Dangaard Brouer <brouer@redhat.com>
2019-02-06 17:58:21 +0100

    tc: common functions.sh used by scripts in this directory
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- tc/tc_ingress_drop.sh
+++ tc/tc_ingress_drop.sh
@@ -11,23 +11,12 @@
 #
 # Author: Jesper Dangaard Brouer <netoptimizer@brouer.com>
 # License: GPLv2
+#
+basedir=`dirname $0`
+source ${basedir}/functions.sh
 
 export TC=/sbin/tc
 
-# sudo trick
-function root_check_run_with_sudo() {
-    # Trick so, program can be run as normal user, will just use "sudo"
-    #  call as root_check_run_as_sudo "$@"
-    if [ "$EUID" -ne 0 ]; then
-	if [ -x $0 ]; then # Directly executable use sudo
-	    echo "# (Not root, running with sudo)" >&2
-            sudo "$0" "$@"
-            exit $?
-	fi
-	echo "cannot perform sudo run of $0"
-	exit 1
-    fi
-}
 root_check_run_with_sudo "$@"
 
 function usage() {
@@ -43,50 +32,6 @@ function usage() {
     echo "  --icmp         : (\$ICMP)      Drop all ICMP traffic"
     echo ""
 }
-
-## -- General shell logging cmds --
-function err() {
-    local exitcode=$1
-    shift
-    echo "ERROR: $@" >&2
-    exit $exitcode
-}
-
-function warn() {
-    echo "WARN : $@" >&2
-}
-
-function info() {
-    if [[ -n "$VERBOSE" ]]; then
-	echo "# $@"
-    fi
-}
-
-# Wrapper call for TC
-function _call_tc() {
-    local allow_fail="$1"
-    shift
-    if [[ -n "$VERBOSE" ]]; then
-	echo "tc $@"
-    fi
-    if [[ -n "$DRYRUN" ]]; then
-	return
-    fi
-    $TC "$@"
-    local status=$?
-    if (( $status != 0 )); then
-	if [[ "$allow_fail" == "" ]]; then
-	    err 3 "Exec error($status) occurred cmd: \"$TC $@\""
-	fi
-    fi
-}
-function call_tc() {
-    _call_tc "" "$@"
-}
-function call_tc_allow_fail() {
-    _call_tc "allow_fail" "$@"
-}
-
 
 # Using external program "getopt" to get --long-options
 OPTIONS=$(getopt -o vfshd: \
